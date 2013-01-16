d21e0fc66afadc389cd96329dccf78f3c91f4687
David Ruyer <david.ruyer@gmail.com>
2013-01-17 00:35:34 +0100

    environment specific redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,7 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS ||= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# Load the redis.yml configuration file 
+redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env].symbolize_keys
+
+# Connect to Redis using the redis_config host and port 
+if redis_config 
+  REDIS = Redis.new(redis_config)
+end
